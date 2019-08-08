f5f7eaea532bc9653bd55bda1dabb36a9b8a3af2
VolksRat71 <nathanieljryan1994@gmail.com>
2019-08-08 15:48:40 -0700

    command delays for udp waait file created

---
+++ backend/commandDelays.js
@@ -0,0 +1,21 @@
+const commandDelays = {
+    command: 500,
+    takeoff: 1000,
+    land: 1000,
+    up: 3000,
+    down: 3000,
+    left: 2000,
+    go: 3000,
+    right: 3000,
+    forward: 3000,
+    back: 3000,
+    cw: 5000,
+    ccw: 5000,
+    flip: 3000,
+    speed: 3000,
+    'battery?': 500,
+    'speed?': 500,
+    'time?': 500,
+};
+
+module.exports = commandDelays;
--- backend/fly.js
+++ backend/fly.js
@@ -1,5 +1,7 @@
 const dgram = require("dgram");
 const wait = require("waait");
+const commandDelays = require("./commandDelays");
+
 
 const PORT = 8889;
 const HOST = "192.168.10.1";
@@ -8,7 +10,7 @@ const drone = dgram.createSocket("udp4");
 drone.bind(PORT);
 
 drone.on("message", message => {
-    console.log(`🤖: ${message}`);
+    console.log(`🚁: ${message}`);
 });
 
 function handleError(err) {
@@ -18,4 +20,18 @@ function handleError(err) {
     }
 };
 
-drone.send("takeoff", 0, 8, PORT, HOST, handleError);
+// drone.send("takeoff", 0, 8, PORT, HOST, handleError);
+
+const commands = ["command", "battery?", "takeoff", "land"];
+
+async function go() {
+    const command = commands[i];
+    const delay = commandDelays[command];
+    console.log(`running command ${command}`);
+    drone.send(command, 0, command.length, PORT, HOST, handleError);
+    await wait(delay);
+    if (i < commands.length) {
+        return go()
+    }
+    console.log("command exicuted");
+}
