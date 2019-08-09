5c6fc2189cfdd83370a00f4ae5b53afd33ee8e00
VolksRat71 <nathanieljryan1994@gmail.com>
2019-08-09 11:23:28 -0700

    prebuilt demonstraton built, mostly just seeing if drone state works

--- backend/commandDelays.js
+++ backend/commandDelays.js
@@ -9,13 +9,16 @@ const commandDelays = {
     right: 5000,
     forward: 5000,
     back: 5000,
-    cw: 5000,
-    ccw: 5000,
-    flip: 3000,
+    cw: 10000,
+    ccw: 10000,
+    flip: 10000,
     speed: 3000,
-    'battery?': 500,
-    'speed?': 500,
-    'time?': 500,
+    "battery?": 500,
+    "speed?": 500,
+    "time?": 500,
+    "temp?": 2000,
+    "height?": 500,
+    "flip f": 7000
 };
 
 module.exports = commandDelays;
--- backend/fly.js
+++ backend/fly.js
@@ -2,16 +2,26 @@ const dgram = require("dgram");
 const wait = require("waait");
 const commandDelays = require("./commandDelays");
 
-
+// req & res address for drone functions
 const PORT = 8889;
 const HOST = "192.168.10.1";
 
 const drone = dgram.createSocket("udp4");
 drone.bind(PORT);
 
+// // req & res address for drone state
+// const droneState = dgram.createSocket("udp4");
+// droneState.bind(8890);
+
+// res from drone for function error or completeion
 drone.on("message", message => {
     console.log(`🚁: ${message}`);
 });
+
+// res from drone for state
+// droneState.on("message", message => {
+//     console.log(`🚁: ${message}`);
+// });
 
 function handleError(err) {
     if (err) {
@@ -20,21 +30,27 @@ function handleError(err) {
     }
 };
 
-const commands = ["command", "battery?", "takeoff", "land"];
+// Prebuilt demonstration
+function automation() {
+    // Array auto-sequence  
+    const commands = ["command", "battery?", "height?", "takeoff", "height?", "flip f", "land", "temp?"];
+    // Begain loop, const delays refers to commandDelay.js for time intervals
+    let i = 0;
 
-let i = 0;
+    // Auto take-off flip & land
+    async function go() {
+        const command = commands[i];
+        const delay = commandDelays[command];
+        console.log(`requesting command: ${command}`);
+        drone.send(command, 0, command.length, PORT, HOST, handleError);
+        await wait(delay);
+        i += 1;
+        if (i < commands.length) {
+            return go();
+        }
+        // After array has been completed console.log
+        console.log("demonstration finished!");
+    };
 
-drone.send('command', 0, 'command'.length, PORT, HOST, handleError);
-
-async function go() {
-    const command = commands[i];
-    const delay = commandDelays[command];
-    console.log(`requesting command: ${command}`);
-    drone.send(command, 0, command.length, PORT, HOST, handleError);
-    await wait(delay);
-    i += 1;
-    if (i < commands.length) {
-        return go();
-    }
-    console.log('demonstration finished!');
+    go();
 };
