bd71281a59d3d4cce209291f42968a0f3fe1a5c0
VolksRat71 <nathanieljryan1994@gmail.com>
2021-03-31 15:02:25 -0500

    chinging the socket to private ip

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,5 +1,8 @@
 import io from "socket.io-client";
+const localnetwork = '192.168.1.'
+const gamingpc = '67'
+const raspberrypi = '130'
 
-const socket = io("http://localhost:6767");
+const socket = io(`http://${localnetwork}${raspberrypi}:6767`);
 
 export default socket;
