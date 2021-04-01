e07c9a1e9b9e3f5f31f00665698a3ddee46f3557
VolksRat71 <nathanieljryan1994@gmail.com>
2021-03-31 19:10:43 -0500

    changing back to a PORT, a version of this app has been added to twitch project

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,8 +1,5 @@
-import io from "socket.io-client";
-const localnetwork = '192.168.1.'
-const gamingpc = '67'
-const raspberrypi = '130'
+import io from 'socket.io-client';
 
-const socket = io(`http://${localnetwork}${raspberrypi}:6767`);
+const socket = io('http://localhost:6767');
 
 export default socket;
