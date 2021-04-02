00552c7547370771ec9712af182d94099389ca8a
VolksRat71 <nathanieljryan1994@gmail.com>
2021-04-01 21:15:24 -0500

    lets see if we can get this puppy online

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,5 +1,6 @@
 import io from 'socket.io-client';
+require('dotenv').config();
 
-const socket = io('http://localhost:6767');
+const socket = io(process.env.REACT_APP_LOCAL_SERVER);
 
 export default socket;
